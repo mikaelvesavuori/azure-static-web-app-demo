c2f70a7713fc10839ec13399b138e28b570e51c9
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-05-22 17:22:24 +0200

    Update

--- src/containers/DemoView/index.jsx
+++ src/containers/DemoView/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import {  withRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import View from 'containers/View';
@@ -7,21 +7,18 @@ import View from 'containers/View';
 import H1 from 'components/H1';
 import Paragraph from 'components/Paragraph';
 
-const ThisView = () => (
-	<View title="Demo Site – This View">
+const DemoView = () => (
+	<View title="Azure Static Web App demo">
 		<Helmet>
 			<meta property="og:site_name" content="Azure Static Web App demo" />
 			<meta property="og:title" content="Azure Static Web App demo" />
 			<meta property="og:description" content="Basic demonstration of an Azure Static Web App using React." />
 		</Helmet>
-		<H1>ThisView</H1>
+		<H1>Azure Static Web App demo</H1>
 		<Paragraph>
-			<Link to="/thisview">Link to ThisView</Link>
-		</Paragraph>
-		<Paragraph>
-			<Link to="/thatview">Link to ThatView</Link>
+			Congratulations! Seems like your Azure Static Web App is running fine.
 		</Paragraph>
 	</View>
 );
 
-export default withRouter(ThisView);
+export default withRouter(DemoView);
